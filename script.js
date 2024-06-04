153b368adebef0d5a89b3c3bc4e88cf6e68fb401
neo <139451447+yonathanjj@users.noreply.github.com>
2024-06-04 09:13:50 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.about-service .card');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
